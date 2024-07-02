7c3b5ef813b37d3cdd6a764878c586bd1175c128
Lautaro Companys <lautarocompanys@gmail.com>
2024-07-02 11:25:36 -0300

    version 1

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', function() {
+    var content = document.querySelector('.header');
+    var scrollPosition = window.scrollY;
+    var targetPosition = 100; // Posición de scroll en píxeles para aplicar los estilos
+  
+    if (scrollPosition > targetPosition) {
+        content.classList.add('scroll-styles');
+    } else {
+        content.classList.remove('scroll-styles');
+    }
+  });
+
